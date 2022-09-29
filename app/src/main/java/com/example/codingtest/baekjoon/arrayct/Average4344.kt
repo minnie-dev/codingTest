2279b3b1d772e594dbafc5a5f34befc15df489d2
minnie <jung.dev98@gmail.com>
2022-09-29 17:52:54 +0900

    220929 평균은 넘겠지 4344 수정

--- app/src/main/java/com/example/codingtest/baekjoon/arrayct/Average4344.kt
+++ app/src/main/java/com/example/codingtest/baekjoon/arrayct/Average4344.kt
@@ -1,19 +1,20 @@
 package com.example.codingtest.baekjoon.arrayct
 
 import java.lang.Math.round
-import java.util.*
+import java.util.Scanner
 import kotlin.math.roundToInt
 
 class Average4344 {
-    fun main() = with(Scanner(System.`in`)) {
-        val tcCount = nextInt() // tc 수
+    fun main() {
+        val scanner = Scanner(System.`in`)
+        val tcCount = scanner.nextInt() // tc 수
         val scoreArray = mutableListOf<Array<Int>>() // 학생 수 , 점수
 
         for (cnt in 0..tcCount) {
-            var c = nextInt()
+            var c = scanner.nextInt()
             var sc : Array<Int>? = arrayOf()
             for(i in 0 until c){
-                sc?.set(i, nextInt())
+                sc?.set(i, scanner.nextInt())
             }
             if (sc != null) {
                 scoreArray.add(sc)

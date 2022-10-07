b185924f77f4c0d11d2f3bf4e57441b3050aacb4
minnie <jung.dev98@gmail.com>
2022-10-07 16:45:10 +0900

    221007 평균1546 성공

--- app/src/main/java/com/example/codingtest/baekjoon/arrayct/avg1546.kt
+++ app/src/main/java/com/example/codingtest/baekjoon/arrayct/avg1546.kt
@@ -3,12 +3,12 @@ package com.example.codingtest.baekjoon.arrayct
 class avg1546 {
     fun main() = with(System.`in`.bufferedReader()){
         val cnt = readLine().toInt()
-        val scoreList = readLine().split(" ")
+        val scoreList = readLine().split(" ").map { it.toDouble() }
 
         val max = scoreList.maxOf { it }
         var sum = 0.0
         for(score in scoreList){
-            sum += score.toDouble()/max.toDouble()*100.0
+            sum += score/max*100
         }
         println(sum/cnt)
     }

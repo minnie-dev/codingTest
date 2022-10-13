3db100e3ec44bbe78e66cd585321d35e23e5fa34
minnie <jung.dev98@gmail.com>
2022-10-13 16:50:55 +0900

    221013 손익분기점1712 성공

--- app/src/main/java/com/example/codingtest/baekjoon/math/BreakEven1712.kt
+++ app/src/main/java/com/example/codingtest/baekjoon/math/BreakEven1712.kt
@@ -3,17 +3,15 @@ package com.example.codingtest.baekjoon.math
 class BreakEven1712 {
     fun main() = with(System.`in`.bufferedReader()) {
         val numList = readLine().split(" ").map { it.toInt() }
-        var cnt = 0
-        for(i in 1..270000000){
-            val value = (numList[2]*i)/(numList[0]+(numList[1]*i)).toDouble()
-            println(value)
-            if(value>1){
-                cnt = i
-                break
-            } else if(value<1 && i == 270000000){
-                cnt = -1
+        if (numList[1] >= numList[2]) {
+            println("-1")
+        } else {
+            val cnt = numList[0] / (numList[2] - numList[1])
+            if (numList[2] * cnt > numList[0] + numList[1] * cnt) {
+                println(cnt)
+            } else {
+                println(cnt + 1)
             }
         }
-        println(cnt)
     }
 }
